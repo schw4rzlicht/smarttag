104a28162ba0def5c0eb794361a198c0a6b3b383
Julian Rabe <mail@julianrabe.com>
2020-05-01 01:36:18 +0200

    Updated file structure

--- gulpfile.js
+++ gulpfile.js
@@ -8,63 +8,86 @@ const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 
 function sassDev() {
-    return gulp.src('css/*.scss')
+    return gulp.src('src/scss/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('dist/css'));
 }
 
 function sassProduction() {
-    return gulp.src('css/*.scss')
+    return gulp.src('src/scss/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(stripCssComments({preserve: false}))
         .pipe(autoprefixer())
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('dist/css'));
 }
 
 function minifyJs() {
-    return gulp.src('js/*.js')
+    return gulp.src('dist/js/**/*.js')
         .pipe(terser())
-        .pipe(gulp.dest('js'));
-}
-
-function watchSass() {
-    return gulp.watch('css/**/*.scss',
-        gulp.series(
-            sassDev
-        )
-    )
+        .pipe(gulp.dest('dist/js'));
 }
 
 function doBrowserify() {
-    return browserify({entries: 'js/index.js'})
+    return browserify({entries: 'src/js/index.js'})
         .bundle()
         .pipe(source('index.js'))
         .pipe(gulp.dest('dist/js'))
 }
 
+function html() {
+    return gulp.src('src/html/**/*.html')
+        .pipe(gulp.dest('dist'));
+}
+
+function watchSass() {
+    return gulp.watch('src/css/**/*.scss',
+        gulp.series(
+            sassDev
+        )
+    );
+}
+
 function watchJs() {
-    return gulp.watch('js/index.js',
+    return gulp.watch('src/js/**/*.js',
         gulp.series(
             doBrowserify
+        )
+    );
+}
+
+function watchHtml() {
+    return gulp.watch('src/**/*.html',
+        gulp.series(
+            html
         )
     )
 }
 
 gulp.task(
     'watch',
-    gulp.series(
-        sassDev,
-        watchSass
+    gulp.parallel(
+        gulp.series(
+            sassDev,
+            watchSass),
+        gulp.series(
+            doBrowserify,
+            watchJs
+        ),
+        gulp.series(
+            html,
+            watchHtml
+        )
     )
 );
 
 gulp.task(
     'buildDev',
     gulp.series(
-        sassDev
+        sassDev,
+        doBrowserify
     )
 );
 
@@ -72,14 +95,7 @@ gulp.task(
     'buildProduction',
     gulp.series(
         sassProduction,
-        minifyJs
-    )
-)
-
-gulp.task(
-    'watchJs',
-    gulp.series(
         doBrowserify,
-        watchJs
+        minifyJs
     )
 )
